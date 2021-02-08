3414ff91401f7b1852c3f2f75f52c415d4d0039d
Seif Elidn <59258154+seifEldinRefaat@users.noreply.github.com>
2021-02-09 01:54:39 +0200

    build architecture pattern MVVM

--- app/src/main/java/com/example/spacechat/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/spacechat/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.spacechat
+package com.example.spacechat.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.spacechat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
