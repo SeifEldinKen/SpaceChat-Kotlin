9a91e16d3e0ab36b200443216517172f58406ee1
Seif Elidn <59258154+seifEldinRefaat@users.noreply.github.com>
2021-02-09 02:01:39 +0200

    create user model data class

---
+++ app/src/main/java/com/example/spacechat/model/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.spacechat.model
+
+data class UserModel(
+    var username: String = "",
+    var email: String = "",
+    var password: String = "",
+    var profileImage: String = "",
+    var userId: String = ""
+)
